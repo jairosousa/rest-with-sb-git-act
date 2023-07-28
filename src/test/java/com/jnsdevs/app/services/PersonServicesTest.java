7ffd373aeba6db41137301b2c5e434e17f8ede1f
Jairo Nascimento <jaironsousa@gmail.com>
2023-07-28 18:04:21 -0300

    :rocket: Java Continuos Integration -  Testando a camada de Serviços   - Testando o Lançamento de Exceção no Método save de PersonService

--- src/test/java/com/jnsdevs/app/services/PersonServicesTest.java
+++ src/test/java/com/jnsdevs/app/services/PersonServicesTest.java
@@ -1,12 +1,12 @@
 package com.jnsdevs.app.services;
 
+import com.jnsdevs.app.exceptions.ResourceNotFoundException;
 import com.jnsdevs.app.model.Person;
 import com.jnsdevs.app.repositories.PersonRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -14,8 +14,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 /**
  * @Autor Jairo Nascimento
@@ -44,7 +47,7 @@ public class PersonServicesTest {
                 "jairo@email.com");
     }
 
-    @DisplayName("Given Person Object whenSavePerson then Return Person Object")
+    @DisplayName("Given Person Object when Sav ePerson then Return Person Object")
     @Test
     void testGivenPersonObject_whenSavePerson_thenReturnPersonObject() {
         // Given
@@ -56,7 +59,26 @@ public class PersonServicesTest {
 
         //Then
         assertNotNull(savedPerson);
-        assertEquals("Jairo", savedPerson.getFirstName());;
+        assertEquals("Jairo", savedPerson.getFirstName());
 
     }
+
+    @DisplayName("Given ExistEmail when Save Person Object then Return Trows Exception")
+    @Test
+    void testGivenExistEmail_whenSavePersonObject_thenTrowsException() {
+        // Given
+        given(personRepository.findByEmail(anyString())).willReturn(Optional.of(person));
+
+        //When
+        var exception = assertThrows(ResourceNotFoundException.class, () -> {
+            personServices.create(person);
+        });
+
+        //Then
+        assertNotNull(exception);
+        assertEquals("Person already exist given e-mail: " + person.getEmail(), exception.getMessage());
+        verify(personRepository, never()).save(any(Person.class));
+
+    }
+
 }
