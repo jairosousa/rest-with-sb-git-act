77b81fab1e59379bd33f8cbe4b9a963e11840808
Jairo Nascimento <jaironsousa@gmail.com>
2023-07-29 16:50:33 -0300

    :rocket: Java Continuos Integration -  Testando os controller   - Testando a Operação de create

---
+++ src/test/java/com/jnsdevs/app/controller/PersonControllerTest.java
@@ -0,0 +1,78 @@
+package com.jnsdevs.app.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jnsdevs.app.model.Person;
+import com.jnsdevs.app.services.PersonServices;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 29/07/2023 - 15:57
+ */
+@WebMvcTest
+class PersonControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @MockBean
+    private PersonServices personServices;
+
+    private Person person;
+
+    @BeforeEach
+    public void setup() {
+        // Given / Arrange
+        person = new Person(
+                "Jairo",
+                "Nascimento",
+                "Uberlandia",
+                "Male",
+                "jairo@email.com");
+    }
+
+    @Test
+    @DisplayName("JUnit test for Given Person Object When Create Person then Return Saved Person")
+    void testGivenPersonObject_WhenCreatePerson_thenReturnSavedPerson() throws Exception {
+
+        //Given / Arrange
+        given(personServices.create(any(Person.class)))
+                .willAnswer((invocation -> invocation.getArgument(0)));
+
+        //When / Act
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.post("/api/person")
+                .contentType(APPLICATION_JSON)
+                .content(mapper
+                        .writeValueAsString(person)));
+
+        //Then / Assert
+        response.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName", is(person.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(person.getLastName())))
+                .andExpect(jsonPath("$.email", is(person.getEmail())));
+
+    }
+
+}
