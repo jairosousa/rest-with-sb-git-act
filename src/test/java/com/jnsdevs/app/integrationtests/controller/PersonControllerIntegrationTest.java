6cb6bb73e856bcc894d08d6c6dfadd326382f053
Jairo Nascimento <jaironsousa@gmail.com>
2023-07-30 19:29:08 -0300

    :rocket: Java Continuos Integration -  Testes de Integração com TestContainers   - Testando a Operação de Create

--- src/test/java/com/jnsdevs/app/integrationtests/controller/PersonControllerIntegrationTest.java
+++ src/test/java/com/jnsdevs/app/integrationtests/controller/PersonControllerIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.jnsdevs.app.integrationtests.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jnsdevs.app.config.TestConfigs;
@@ -10,10 +11,12 @@ import io.restassured.filter.log.LogDetail;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.*;
 
 /**
  * @Autor Jairo Nascimento
@@ -31,13 +34,13 @@ public class PersonControllerIntegrationTest extends AbstractIntegrationTest {
     private static Person person;
 
     @BeforeAll
-    public void setup() {
+    public static void setup() {
         // Given / Arrange
         mapper = new ObjectMapper()
                 .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
         specification = new RequestSpecBuilder()
-                .setBasePath("")
+                .setBasePath("/api/person")
                 .setPort(TestConfigs.SERVER_PORT)
                 .addFilter(new RequestLoggingFilter(LogDetail.ALL))
                 .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
@@ -51,5 +54,40 @@ public class PersonControllerIntegrationTest extends AbstractIntegrationTest {
                 "jairo@email.com");
     }
 
+    @Test
+    @Order(1)
+    @DisplayName("JUNIT integration Test Given Person Object when Create One Person Should Return A Person Object")
+    void integrationTestGivenPersonObject_when_CreateOnePerson_ShouldReturnAPersonObject() throws JsonProcessingException {
+        var content = given()
+                .spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .body(person)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        Person createPerson = mapper.readValue(content, Person.class);
+
+        person = createPerson;
+
+        assertNotNull(createPerson);
+        assertNotNull(createPerson.getId());
+        assertNotNull(createPerson.getFirstName());
+        assertNotNull(createPerson.getLastName());
+        assertNotNull(createPerson.getAddress());
+        assertNotNull(createPerson.getGender());
+        assertNotNull(createPerson.getEmail());
+
+        assertTrue(createPerson.getId() > 0);
+        assertEquals("Jairo", createPerson.getFirstName());
+        assertEquals("Nascimento", createPerson.getLastName());
+        assertEquals("Uberlandia", createPerson.getAddress());
+        assertEquals("Male", createPerson.getGender());
+        assertEquals("jairo@email.com", createPerson.getEmail());
+    }
 
 }
